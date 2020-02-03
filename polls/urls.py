34240d09a081079a65505acdbf2569e79f18cd10
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2020-02-03 12:51:36 +0900

    3章の「実際に動作するビューを書く」まで

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #例) /pools/
     path('', views.index, name='index'),
-    path('<int:number>', views.subview, name='suburl'),
+    #例) /pools/5/
+    path('<int:question_id>', views.detail, name='detail'),
+    #例) /pools/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    #例) /pools/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 
-def index(request):
-    urlName = reverse('index')
-    return HttpResponse("Hello World{0}".format(urlName))
+from .models import Question
 
-def subview(request, number):
-    urlName = reverse('suburl', args=[number])
-    return HttpResponse("これはsubViewデス{0} url={1}".format(number, urlName))
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+# def subview(request, number):
+#     urlName = reverse('suburl', args=[number])
+#     return HttpResponse("これはsubViewデス{0} url={1}".format(number, urlName))
+
+def detail(request, question_id):
+    return HttpResponse("You're lokking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're lokking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
